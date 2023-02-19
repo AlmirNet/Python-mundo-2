5bf483ec64028ee45ec5cf45883634390aa40e08
AlmirNET <107294384+AlmirNet@users.noreply.github.com>
2023-02-19 19:06:29 -0300

    .

--- 67.exteste2.py
+++
@@ -1,7 +0,0 @@
-while True:
-    n = int(input('Você quer ver tabuada de qual valor: '))
-    if n < 0:
-        break
-    for c in range(1,11):
-        print(f'{n} X {c} = {n*c}')
-print('PROGRAMA DE TABUADA ENCERRADO,É MUITO AGRADAVEL!')
